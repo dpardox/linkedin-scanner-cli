6a059e87eab1d338ebab80de503f2144814779dd
Donovan Pardo <dpardox@gmail.com>
2025-05-22 19:44:23 -0500

    Refactor job search methods for improved error handling and code clarity; remove try-catch blocks where unnecessary

--- src/core/pages/jobs-search.page.ts
+++ src/core/pages/jobs-search.page.ts
@@ -46,75 +46,62 @@ export class JobsSearchPage extends BasePage { // TODO (dpardo): pages should be
   }
 
   public async noJobsFound(): Promise<boolean> {
-    try {
-      await this.page.waitForTimeout(randms());
-      const locator = await this.page.getByRole('paragraph').filter({ hasText: 'No matching jobs found.' });
-      const visible = await locator.isVisible();
+    await this.page.waitForTimeout(randms());
+    const locator = this.page.getByRole('paragraph').filter({ hasText: 'No matching jobs found.' });
+    const visible = await locator.isVisible();
 
-      if (visible) {
-        this.logger.error('No matching jobs found.');
-        return true;
-      }
-
-      return false;
-    } catch (error) {
-      this.logger.error('Error checking for no jobs found: %s', error);
-      await this.page.pause();
-      process.exit(1);
-
+    if (visible) {
+      this.logger.error('No matching jobs found.');
+      return true;
     }
+
+    return false;
   }
 
   public async getJobIds(): Promise<string[]> {
-    try {
-      this.logger.info('Retrieving job listings...');
+    this.logger.info('Retrieving job listings...');
 
-      const layout = '.scaffold-layout__list';
-      const scroll = `${layout} > div`;
-      const list = `${scroll} ul`;
-      const item = `${list} .scaffold-layout__list-item`;
-      const attr = 'data-occludable-job-id';
+    const layout = '.scaffold-layout__list';
+    const scroll = `${layout} > div`;
+    const list = `${scroll} ul`;
+    const item = `${list} .scaffold-layout__list-item`;
+    const attr = 'data-occludable-job-id';
 
-      await this.page.waitForTimeout(randms());
+    await this.page.waitForTimeout(randms());
 
-      await this.page.waitForSelector(layout);
-      await this.page.waitForSelector(scroll);
+    await this.page.waitForSelector(layout);
+    await this.page.waitForSelector(scroll);
 
-      await this.page.waitForTimeout(randms());
+    await this.page.waitForTimeout(randms());
 
-      await this.scrollToBottom(scroll);
+    await this.scrollToBottom(scroll);
 
-      await this.page.waitForSelector(list);
-      await this.page.waitForSelector(item);
+    await this.page.waitForSelector(list);
+    await this.page.waitForSelector(item);
 
-      await this.page.waitForTimeout(randms());
+    await this.page.waitForTimeout(randms());
 
-      const jobs = await this.page.$$(item);
+    const jobs = await this.page.$$(item);
 
-      const ids: string[] = [];
-      for (const job of jobs) {
-        await job.waitForElementState('visible');
-        await job.waitForElementState('stable');
-        await job.scrollIntoViewIfNeeded();
-        await job.hover();
-        const id = await job.getAttribute(attr);
-        id && ids.push(id);
-      }
-
-      await this.page.evaluate((selector) => {
-        const el = document.querySelector(selector);
-        if (el) {
-          el.scrollTop = 0;
-        }
-      }, scroll);
-
-      this.logger.info(`Found %s job(s).`, ids.length);
-      return ids;
-    } catch (error) {
-      this.logger.error('Error getting jobs: %s', error);
-      await this.page.pause();
-      return [];
+    const ids: string[] = [];
+    for (const job of jobs) {
+      await job.waitForElementState('visible');
+      await job.waitForElementState('stable');
+      await job.scrollIntoViewIfNeeded();
+      await job.hover();
+      const id = await job.getAttribute(attr);
+      id && ids.push(id);
     }
+
+    await this.page.evaluate((selector) => {
+      const el = document.querySelector(selector);
+      if (el) {
+        el.scrollTop = 0;
+      }
+    }, scroll);
+
+    this.logger.info(`Found %s job(s).`, ids.length);
+    return ids;
   }
 
   public async nextPage(): Promise<boolean> {
@@ -155,198 +142,150 @@ export class JobsSearchPage extends BasePage { // TODO (dpardo): pages should be
   }
 
   public async isDissmissedJob(job: string): Promise<boolean> {
-    try {
-      const selector = `.job-card-container[data-job-id="${job}"].job-card-list--is-dismissed`;
-      const dismissed = await this.page.$(selector);
+    const selector = `.job-card-container[data-job-id="${job}"].job-card-list--is-dismissed`;
+    const dismissed = await this.page.$(selector);
 
-      if (dismissed) {
-        dismissed.scrollIntoViewIfNeeded();
-        this.logger.warn(`Already dissmissed job "%s"`, await this.getJobTitle(job));
-        await this.setBackgroundColor(job, 'rgba(140, 140, 140, .2)');
-        return true;
-      }
-
-      return false;
-    } catch (error) {
-      this.logger.error('Error checking for dismissed job: %s', error);
-      await this.page.pause();
-      process.exit(1);
+    if (dismissed) {
+      dismissed.scrollIntoViewIfNeeded();
+      this.logger.warn(`Already dissmissed job "%s"`, await this.getJobTitle(job));
+      await this.setBackgroundColor(job, 'rgba(140, 140, 140, .2)');
+      return true;
     }
+
+    return false;
   }
 
   public async isAppliedJob(job: string): Promise<boolean> {
-    try {
-      await this.page.waitForTimeout(randms());
-      const selector = `.job-card-container[data-job-id="${job}"] .job-card-container__footer-job-state`;
-      let el = await this.page.$(selector);
+    await this.page.waitForTimeout(randms());
+    const selector = `.job-card-container[data-job-id="${job}"] .job-card-container__footer-job-state`;
+    let el = await this.page.$(selector);
 
-      if (!el) return false;
+    if (!el) return false;
 
-      const text = await el.evaluate(el => el.textContent ?? '');
-      const applied = normalize(text);
+    const text = await el.evaluate(el => el.textContent ?? '');
+    const applied = normalize(text);
 
-      if (applied?.includes('applied')) {
-        this.logger.warn(`You already applied to job "%s".`, await this.getJobTitle(job));
-        await this.dissmissJob(job);
-        return true;
-      };
+    if (applied?.includes('applied')) {
+      this.logger.warn(`You already applied to job "%s".`, await this.getJobTitle(job));
+      await this.dissmissJob(job);
+      return true;
+    };
 
-      return false;
-    } catch (error) {
-      this.logger.error('Error checking for applied job: %s', error);
-      await this.page.pause();
-      process.exit(1);
-    }
+    return false;
   }
 
   public async isEmptyJob(job: string): Promise<boolean> {
-    try {
-      await this.page.waitForTimeout(randms());
-      const selector = `.scaffold-layout__list-item[data-occludable-job-id="${job}"]`;
-      let text = await this.page.$eval(selector, (el) => el.textContent);
-      text &&= normalize(text);
+    await this.page.waitForTimeout(randms());
+    const selector = `.scaffold-layout__list-item[data-occludable-job-id="${job}"]`;
+    let text = await this.page.$eval(selector, (el) => el.textContent);
+    text &&= normalize(text);
 
-      if (!text) {
-        this.logger.error('Empty job listing found.');
-        return true;
-      }
-
-      return false;
-    } catch (error) {
-      this.logger.error('Error checking for empty job: %s', error);
-      await this.page.pause();
-      process.exit(1);
+    if (!text) {
+      this.logger.error('Empty job listing found.');
+      return true;
     }
+
+    return false;
   }
 
   private async alreadySelected(job: string, wait = true): Promise<boolean> {
-    try {
-      const selector = `.job-details-jobs-unified-top-card__job-title a[href*="/jobs/view/${job}"]`;
-      wait && await this.page.waitForSelector(selector);
-      const el = await this.page.$(selector);
-      return !!el;
-    } catch (error) {
-      this.logger.error('Error checking for already selected job: %s', error);
-      await this.page.pause();
-      process.exit(1);
-    }
+    const selector = `.job-details-jobs-unified-top-card__job-title a[href*="/jobs/view/${job}"]`;
+    wait && await this.page.waitForSelector(selector);
+    const el = await this.page.$(selector);
+    return !!el;
   }
 
   public async selectJob(job: string): Promise<void> {
-    try {
-      this.logger.info('Selecting job "%s"...', job);
+    this.logger.info('Selecting job "%s"...', job);
 
-      await this.page.waitForTimeout(randms());
+    await this.page.waitForTimeout(randms());
 
-      if (await this.alreadySelected(job, false)) {
-        this.logger.info('Already selected job "%s".', job);
-        return;
-      }
-
-      const selector = `.job-card-container[data-job-id="${job}"]`;
-      await this.page.waitForSelector(selector);
-      const item = await this.page.$(selector);
-
-      if (!item) {
-        this.logger.error('Job card not found.');
-        return;
-      }
-
-      await item.waitForElementState('visible');
-      await item.waitForElementState('stable');
-
-      await this.page.waitForTimeout(randms());
-
-      await item.scrollIntoViewIfNeeded();
-      await this.page.waitForTimeout(randms());
-      await item.hover();
-      await this.page.waitForTimeout(randms());
-      await item.click({ force: true });
-      await this.page.waitForTimeout(randms());
-
-      this.logger.info(`Selected job "%s"`, await this.getJobTitle(job));
-      await this.setBackgroundColor(job, 'lightyellow');
-
-      await this.alreadySelected(job);
-    } catch (error) {
-      this.logger.error('Error selecting job: %s', error);
-      await this.page.pause();
-      process.exit(1);
+    if (await this.alreadySelected(job, false)) {
+      this.logger.info('Already selected job "%s".', job);
+      return;
     }
+
+    const selector = `.job-card-container[data-job-id="${job}"]`;
+    await this.page.waitForSelector(selector);
+    const item = await this.page.$(selector);
+
+    if (!item) {
+      this.logger.error('Job card not found.');
+      return;
+    }
+
+    await item.waitForElementState('visible');
+    await item.waitForElementState('stable');
+
+    await this.page.waitForTimeout(randms());
+
+    await item.scrollIntoViewIfNeeded();
+    await this.page.waitForTimeout(randms());
+    await item.hover();
+    await this.page.waitForTimeout(randms());
+    await item.click({ force: true });
+    await this.page.waitForTimeout(randms());
+
+    this.logger.info(`Selected job "%s"`, await this.getJobTitle(job));
+    await this.setBackgroundColor(job, 'lightyellow');
+
+    await this.alreadySelected(job);
   }
 
   public async getJobDetails(jobId: string): Promise<JobModel> {
-    try {
-      this.logger.info('Retrieving job details...');
+    this.logger.info('Retrieving job details...');
 
-      await this.page.waitForTimeout(randms());
+    await this.page.waitForTimeout(randms());
 
-      const selector = {
-        title: '.job-details-jobs-unified-top-card__job-title',
-        location: '.job-details-jobs-unified-top-card__tertiary-description-container .tvm__text',
-        description: '.jobs-description__container .jobs-box__html-content div',
-      };
+    const selector = {
+      title: '.job-details-jobs-unified-top-card__job-title',
+      location: '.job-details-jobs-unified-top-card__tertiary-description-container .tvm__text',
+      description: '.jobs-description__container .jobs-box__html-content div',
+    };
 
-      await this.page.waitForSelector(selector.title);
-      await this.page.waitForSelector(selector.location, { state: 'attached' });
-      await this.page.waitForSelector(selector.description);
+    await this.page.waitForSelector(selector.title);
+    await this.page.waitForSelector(selector.location, { state: 'attached' });
+    await this.page.waitForSelector(selector.description);
 
-      await this.page.waitForTimeout(randms());
+    await this.page.waitForTimeout(randms());
 
-      const jobModel = new JobModel();
-      jobModel.id = jobId;
-      jobModel.title = await this.innerText(selector.title);
-      jobModel.location = await this.innerText(selector.location);
-      jobModel.description = await this.innerText(selector.description);
-      jobModel.highSkillsMatch = await this.page.getByText('High skills match', { exact: true }).isVisible().catch(() => false);
-      jobModel.isClosed = await this.page.getByText('No longer accepting applications', { exact: true }).isVisible().catch(() => false);
+    const jobModel = new JobModel();
+    jobModel.id = jobId;
+    jobModel.title = await this.innerText(selector.title);
+    jobModel.location = await this.innerText(selector.location);
+    jobModel.description = await this.innerText(selector.description);
+    jobModel.highSkillsMatch = await this.page.getByText('High skills match', { exact: true }).isVisible().catch(() => false);
+    jobModel.isClosed = await this.page.getByText('No longer accepting applications', { exact: true }).isVisible().catch(() => false);
 
-      return jobModel;
-    } catch (error) {
-      this.logger.error('Error getting job details: %s', error);
-      await this.page.pause();
-      process.exit(1);
-    }
+    return jobModel;
   }
 
   public async dissmissJob(job: string): Promise<void> {
-    try {
-      await this.page.waitForTimeout(randms());
-      this.logger.warn(`Dismissing job "%s"...`, await this.getJobTitle(job));
-      const selector = `.job-card-container[data-job-id="${job}"] .job-card-container__action`;
-      await this.page.waitForSelector(selector);
-      const button = await this.page.$(selector);
+    await this.page.waitForTimeout(randms());
+    this.logger.warn(`Dismissing job "%s"...`, await this.getJobTitle(job));
+    const selector = `.job-card-container[data-job-id="${job}"] .job-card-container__action`;
+    await this.page.waitForSelector(selector);
+    const button = await this.page.$(selector);
 
-      if (!button) {
-        this.logger.error('Dismiss button not found.');
-        return;
-      }
-
-      await button.waitForElementState('visible');
-      await button.waitForElementState('stable');
-      await button.click();
-      await this.setBackgroundColor(job, 'rgba(140, 140, 140, .2)');
-    } catch (error) {
-      this.logger.error('Error dismissing job: %s', error);
-      await this.page.pause();
-      process.exit(1);
+    if (!button) {
+      this.logger.error('Dismiss button not found.');
+      return;
     }
+
+    await button.waitForElementState('visible');
+    await button.waitForElementState('stable');
+    await button.click();
+    await this.setBackgroundColor(job, 'rgba(140, 140, 140, .2)');
   }
 
   public async waitForJobToBeDismissed(job: string) {
-    try {
-      const title = await this.getJobTitle(job);
-      this.logger.warn(`Waiting for "%s"...`, title);
-      await this.setBackgroundColor(job, '#daebd1');
-      const selector = `.job-card-container[data-job-id="${job}"].job-card-list--is-dismissed`;
-      await this.page.waitForSelector(selector, { timeout: 0 });
-      await this.setBackgroundColor(job, 'rgba(140, 140, 140, .2)');
-      this.logger.success(`Job "%s" dismissed!`, title);
-    } catch (error) {
-      this.logger.error('Error waiting for job to be dismissed: %s', error);
-      await this.page.pause();
-      process.exit(1);
-    }
+    const title = await this.getJobTitle(job);
+    this.logger.warn(`Waiting for "%s"...`, title);
+    await this.setBackgroundColor(job, '#daebd1');
+    const selector = `.job-card-container[data-job-id="${job}"].job-card-list--is-dismissed`;
+    await this.page.waitForSelector(selector, { timeout: 0 });
+    await this.setBackgroundColor(job, 'rgba(140, 140, 140, .2)'); // TODO (dpardo): move color to enum
+    this.logger.success(`Job "%s" dismissed!`, title);
   }
 
   public async setBackgroundColor(job: string, color: string) {
