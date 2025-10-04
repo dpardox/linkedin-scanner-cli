ee7f3ed10178353f6f8a504ed02d3cc6d12c8b7b
Donovan Pardo <dpardox@gmail.com>
2025-10-03 20:34:58 -0500

    Add new keyword for Junior .NET Engineer to dotnet keywords list

--- src/config/keywords/dotnet.keywords.ts
+++ src/config/keywords/dotnet.keywords.ts
@@ -40,6 +40,7 @@ export const dotnetKeywords = [
   'Strong background in C#',
   'Language fluency in C#',
   'Familiarity with .NET',
+  'Ingeniero Junior .NET',
   'Conocimiento en .NET',
   'experience with .NET',
   'strong focus on .NET',
